93cf901930165c0d82ad623e5570072fcb2267ac
anirudhmakhana <anirudhmakhana@gmail.com>
2023-11-17 13:44:45 +0530

    [fix]: import

--- contracts/__mocks__/MockERC20.sol
+++ contracts/__mocks__/MockERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MockERC20 is ERC20, ERC20Permit {
     constructor() ERC20("MockERC20", "ERC20") ERC20Permit("MockERC20") {
